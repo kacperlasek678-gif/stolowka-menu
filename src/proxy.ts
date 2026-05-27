03eda12f5eb185a8190ae149fe3fb1f07d4a9ec4
kacperlasek678-gif <kacper.lasek678@gmail.com>
2026-05-27 21:48:34 +0200

    Fix Proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,27 +1,35 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(
+export function proxy(
   request: NextRequest
 ) {
+
   const authenticated =
     request.cookies.get(
       "authenticated"
     );
 
   if (
+    !authenticated &&
     request.nextUrl.pathname.startsWith(
       "/admin"
-    ) &&
-    !authenticated
+    )
   ) {
+
     return NextResponse.redirect(
       new URL(
         "/login",
         request.url
       )
     );
+
   }
 
   return NextResponse.next();
+
 }
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
